db9e1ecb9f38dee9372c2c90155b539f43bc9add
Малова Наталия <nmalova@mail.ru>
2022-12-13 23:03:52 +0300

    HW_6

--- HW_6/task_1.py
+++ HW_6/task_1.py
@@ -2,7 +2,28 @@
 Напишите программу вычисления арифметического выражения заданного строкой.
 Используйте операции +,-,/,*. приоритет операций стандартный.
 По возможности реализуйте использования скобок, меняющих приоритет операций.
-
-Ввод: значение типа <str>
-Вывод: значение числового типа данных
+Ввод: значение типа <str>. Вывод: значение числового типа данных
 """
+def calk(pstr):
+    if "-" in  pstr:
+        part_1, part_2 = pstr.split("-", 1)
+        result = float(calk(part_1)) - float(calk(part_2)) 
+    elif "+" in  pstr:
+        part_1, part_2 = pstr.split("+", 1)
+        result = float(calk(part_1)) + float(calk(part_2))
+    elif "*" in  pstr:
+        part_1, part_2 = pstr.split("*", 1)
+        result = float(calk(part_1)) * float(calk(part_2))
+    elif "/" in  pstr:
+        part_1, part_2 = pstr.split("/", 1)
+        result = float(calk(part_1)) / float(calk(part_2))
+    else:
+        result = float(pstr)
+    return result 
+
+string = input("Введите арифметическое выражение:  ")
+string = ''.join(string.split())
+while "(" in string:
+    ss = string.split('(', 1)[1].split(')')[0]
+    string = string.replace('(' + ss +')', str(calk(ss)))
+print(f'{string} = {calk(string)}')
